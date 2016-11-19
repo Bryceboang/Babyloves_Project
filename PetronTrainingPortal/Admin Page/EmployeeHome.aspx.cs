97e52fb353257230ff1b13acb71d261cb59efb92
John Joshua Reyes <joshuareyes0601@gmail.com>
2016-11-20 07:54:23 +0800

    Added codes for the error in DepartmentHome and EmployeeHome

--- PetronTrainingPortal/Admin Page/EmployeeHome.aspx.cs
+++ PetronTrainingPortal/Admin Page/EmployeeHome.aspx.cs
@@ -11,4 +11,49 @@ public partial class Admin_Page_EmployeeHome : System.Web.UI.Page
     {
 
     }
+
+    public void gridEmployee_RowCommand(Object sender, GridViewCommandEventArgs e)
+    {
+        //using (var context = new DatabaseContext())
+        //{
+        //    string trainingTitle = lblTrainingTitle.Text;
+        //    var selectTrain = context.Trainings.FirstOrDefault(c => c.TrainingTitle == trainingTitle);
+        //    if (e.CommandName == "Remove")
+        //    {
+        //        if (selectTrain != null)
+        //        {
+        //            var checkExisting = context.EmployeeTrainings.FirstOrDefault(c => c.TrainingId == selectTrain.TrainingId);
+        //            var checkExistingComplete = context.CompleteEmployeeTrainings.FirstOrDefault(c => c.TrainingId == selectTrain.TrainingId);
+
+        //            if (checkExisting != null || checkExistingComplete != null)
+        //            {
+        //                Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptkey", "<script>alert('Cannot delete this training because some employees are registered here.');</script>");
+        //            }
+        //            else
+        //            {
+        //                context.Trainings.Remove(selectTrain);
+        //                context.SaveChanges();
+
+        //                Page.ClientScript.RegisterStartupScript(this.GetType(), "scriptkey", "<script>alert('The selected training is removed.');</script>");
+        //                Clear();
+
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (selectTrain != null)
+        //        {
+        //            lblHiddenTrainingCode.Text = selectTrain.TrainingCode;
+        //            txtCode.Text = selectTrain.TrainingCode;
+        //            txtTitle.Text = selectTrain.TrainingTitle;
+        //            txtVenue.Text = selectTrain.Venue;
+        //            txtDateDuration.Text = selectTrain.DateDuration;
+        //            txtTimeDuration.Text = selectTrain.TimeDuration;
+        //            txtProvider.Text = selectTrain.TrainingProvider;
+        //            txtParticipants.Text = selectTrain.TargetParticipants;
+        //        }
+        //    }
+        //}
+    }
 }
